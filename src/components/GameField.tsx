a4f40445248bcd3199f81836069115f8bba2de46
smecik48 <piotrsmet@interia.pl>
2025-07-21 19:50:45 +0200

    updated logic and added ugly styling

--- src/components/GameField.tsx
+++ src/components/GameField.tsx
@@ -6,6 +6,8 @@ export default function GameField(){
     const [currentPlayer, setCurrentPlayer] = useState("X")
     const [disabled, setDisabled] = useState(false)
     const [valuesArr, setValue] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0])
+    const [reset, setReset] = useState(false);
+
     
 
     const click = (player: string, index: number): void => {
@@ -45,16 +47,26 @@ export default function GameField(){
         
     }
 
+    const restart = () => {
+        setValue([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        setDisabled(false);
+        setCurrentPlayer("X");
+        setReset(true);
+        setTimeout(() => setReset(false), 50);
+    };
+
+
     return(
         <>
-            <div className="flex flex-col">
+            <div className="flex flex-col p-15 bg-[#34699A] rounded-2xl justify-center items-center">
                 <div className="w-[33vw] max-w-[430px] min-w-[300px] h-[70px] mb-5 relative">
+                
                     {currentPlayer === "X" && !disabled && (
                         <motion.span
                             initial={{ opacity: 0, x: -20 }}
                             animate={{ opacity: 1, x: 0 }}
                             transition={{ duration: 0.3 }}
-                            className="text-4xl absolute left-0 rotate-330 translate-x-[-20px] font-bold text-[#91C8E4] underline drop-shadow-blue-700 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+                            className="text-4xl absolute left-0 rotate-330 translate-x-[-20px] font-bold underline drop-shadow-blue-700 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] bg-[#FDF5AA] p-1 rounded-lg"
                         >
                             Player X
                         </motion.span>
@@ -65,23 +77,28 @@ export default function GameField(){
                             initial={{ opacity: 0, x: 20}}
                             animate={{ opacity: 1, x: 0}}
                             transition={{ duration: 0.3 }}
-                            className="text-4xl absolute right-0 rotate-30 translate-x-[20px] font-bold text-[#91C8E4] underline"
+                            className="text-4xl absolute right-0 rotate-30 translate-x-[20px] font-bold  underline bg-[#FDF5AA] p-1 rounded-lg"
                         >
                             PLAYER O
                         </motion.span>
                         )
                     }
-
-
-                    
-                    { disabled && <span className="text-4xl absolute bottom-1 left-1/2 -translate-x-1/2 font-bold text-[#91C8E4] text-nowrap underline">
+                    {disabled && (
+                        <motion.span 
+                            initial={{opacity: 0, y: -20}}
+                            animate={{opacity: 1, y: 0}}
+                            transition={{duration: 0.3}}
+                            className="text-4xl absolute bottom-1 left-1/2 -translate-x-1/2 font-bold  bg-[#FDF5AA] p-3 rounded-lg text-nowrap underline"
+                        >
                         {currentPlayer != "none" ? "PLAYER " + currentPlayer + " WINS" : "NO WINNERS"}
-                        </span>
-                    }
+                        </motion.span>
+                    )}
+                    
                 </div>
-                <div className="grid grid-cols-3 gap-4">
-                    {[...Array(9).keys()].map(k => <Tile key={k} id={k} player={currentPlayer} isDisabled={disabled} changePlayer={setCurrentPlayer} click={click}/>)}
+                <div className="grid grid-cols-3 gap-4 p-5 rounded-2xl backdrop-blur-2xl opacity-90">
+                    {[...Array(9).keys()].map(k => <Tile key={k} id={k} player={currentPlayer} isDisabled={disabled} changePlayer={setCurrentPlayer} click={click} reset={reset}/>)}
                 </div>
+                <button onClick={restart} className="bg-[#FDF5AA] p-3 rounded-lg">Restart</button>
             </div>
         </>
     )
--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,21 +1,39 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 interface TileProps{
     id: number,
     player: string,
     isDisabled: boolean,
     changePlayer: React.Dispatch<React.SetStateAction<string>>,
-    click: (arg1: string, id: number) => void
+    click: (arg1: string, id: number) => void,
+    reset: boolean
 }
 
-export default function Tile({id, player, isDisabled, changePlayer, click}: TileProps){
+export default function Tile({id, player, isDisabled, changePlayer, click, reset}: TileProps){
     const [sign, setSign] = useState("")
     const [disabled, setDisabled] = useState(false)
+    const [style, setStyle] = useState("w-[100%] aspect-square bg-[#FDF5AA] font-bold ")
+
+    useEffect(() => {
+        if (reset) {
+            setSign("");
+            setDisabled(false);
+            setStyle("w-[100%] aspect-square bg-[#FDF5AA] font-bold ")
+        }
+    }, [reset]);
+
+    const changeColor = (player: string) => {
+        if(player == "X")
+            setStyle((prev) => {return prev + "text-green-500"})
+        else
+            setStyle((prev) => {return prev + "text-red-500"})
+    }
+
     return(
         <>
             <div className="aspect-square w-[10vw] max-w-[130px] min-w-[90px] flex justify-center align-middle items-center text-3xl">
-                <button onClick={() => {setSign(player); setDisabled(true); click(player, id); changePlayer(player === "X" ? "O" : "X")}}
-                 className="w-[100%] aspect-square bg-[#91C8E4] font-bold" disabled={isDisabled || disabled}>
+                <button onClick={() => {setSign(player); setDisabled(true); click(player, id); changePlayer(player === "X" ? "O" : "X"); changeColor(player)}}
+                 className={style} disabled={isDisabled || disabled}>
                     {sign}
                 </button>
             </div>
